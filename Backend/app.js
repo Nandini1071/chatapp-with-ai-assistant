af13d14d54491ab35b6f0e02dac4753e5b4a456e
Nandini Sabharwal <134420513+Nandini1071@users.noreply.github.com>
2026-01-07 11:24:48 +0530

    code for models

--- Backend/app.js
+++ Backend/app.js
@@ -1,8 +1,10 @@
-import express from 'express'
+import express from "express";
+import connect from "./db/db.js";
 
-const app=express()
+const app = express();
+connect();
 
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 export default app;
---
+++ Backend/db/db.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+import dotenv from 'dotenv'
+dotenv.config()
+
+function connect() {
+  mongoose
+    .connect(process.env.MONGO_URI)
+    .then(() => {
+      console.log("Connected to DB!");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
+
+export default connect;
